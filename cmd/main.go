5897778a0ebad306113668519d723752ab8c5129
Dmitry Belanov <dbelanov@ptsecurity.com>
2026-05-07 17:47:36 +0300

    Add --etcd commandline parameter

--- cmd/main.go
+++ cmd/main.go
@@ -6,20 +6,32 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"cluster-tumbler/internal/config"
 	"cluster-tumbler/internal/runtime"
 )
 
+type repeatedFlag []string
+
+func (f *repeatedFlag) String() string  { return strings.Join(*f, ",") }
+func (f *repeatedFlag) Set(v string) error { *f = append(*f, v); return nil }
+
 func main() {
 	cfgPath := flag.String("config", "config.yaml", "path to config file")
+	var etcdEndpoints repeatedFlag
+	flag.Var(&etcdEndpoints, "etcd", "etcd endpoint address:port (repeatable, overrides etcd.endpoints)")
 	flag.Parse()
 
 	cfg, err := config.Load(*cfgPath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "config error: %v\n", err)
 		os.Exit(1)
+	}
+
+	if len(etcdEndpoints) > 0 {
+		cfg.Etcd.Endpoints = []string(etcdEndpoints)
 	}
 
 	rt, err := runtime.New(cfg)
